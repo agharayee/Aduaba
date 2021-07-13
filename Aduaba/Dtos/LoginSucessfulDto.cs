10b91bdef9a94e616025ad8e3b6a765376e97d94
Bettech123 <you@example.com>
2021-07-13 10:37:52 +0100

    phone number added to login

--- Aduaba/Dtos/LoginSucessfulDto.cs
+++ Aduaba/Dtos/LoginSucessfulDto.cs
@@ -12,5 +12,6 @@ namespace Aduaba.Dtos
         public string Email { get; set; }
         public string  FirstName { get; set; }
         public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
